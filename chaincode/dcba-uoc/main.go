60aeb9ddfc1fdab68cba610ed1519c1bc70230b1
AnkanSaha18 <ankansaha314159@gmail.com>
2026-03-26 21:02:18 +0600

    Chaincode dcba-uoc updated with PARS score and bloome filter

--- chaincode/dcba-uoc/main.go
+++ chaincode/dcba-uoc/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+    "fmt"
+    "github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 func main() {
-	dcsContract      := new(DCSContract)
-	lifecycleContract := new(LifecycleContract)
+    dcsContract      := new(DCSContract)
+    lifecycleContract := new(LifecycleContract)
+    ordersContract    := new(OrdersContract) 
 
-	chaincode, err := contractapi.NewChaincode(dcsContract, lifecycleContract)
-	if err != nil {
-		fmt.Printf("Error creating DCBA UOC chaincode: %s", err)
-		return
-	}
-	if err := chaincode.Start(); err != nil {
-		fmt.Printf("Error starting DCBA UOC chaincode: %s", err)
-	}
+    chaincode, err := contractapi.NewChaincode(
+        dcsContract,
+        lifecycleContract,
+        ordersContract, 
+    )
+    if err != nil {
+        fmt.Printf("Error creating DCBA UOC chaincode: %s", err)
+        return
+    }
+    if err := chaincode.Start(); err != nil {
+        fmt.Printf("Error starting DCBA UOC chaincode: %s", err)
+    }
 }
